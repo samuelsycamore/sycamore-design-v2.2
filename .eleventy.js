c997b1b4284f4a0af087284579b4cd01cebe8cec
Sam Sycamore <samuelsycamore@gmail.com>
2021-08-27 21:15:18 -0700

    making progress

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,5 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/sass");
-  eleventyConfig.addWatchTarget("./src/css/burger.css");
   eleventyConfig.addPassthroughCopy('./src/img');
 
   return {
--- README.md
+++ README.md
@@ -5,7 +5,4 @@
 ### Built With
 - Eleventy (Nunjucks)
 - SCSS
-- GSAP
-- Rellax.js
-- Alpine.js
 - LottieFiles
